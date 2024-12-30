380891684fe87c9840ad69774b316d98321b54a5
OlegEfremov54 <ole_f@mail.ru>
2024-12-30 14:20:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dz16animation/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.dz16animation
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var toolbarMain: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Тулбар
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "  Анимация"
+        toolbarMain.subtitle = "Версия 1. "
+        toolbarMain.setLogo(R.drawable.baseline_engineering_24)
+
+
+    }
+    //Инициализация Меню
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.infoMenuMain -> {
+                Toast.makeText(
+                    applicationContext, "Автор Ефремов О.В. Создан 28.12.2024",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext, "Работа приложения завершена",
+                    Toast.LENGTH_LONG
+                ).show()
+                finishAffinity()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
